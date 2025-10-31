28e982ce913f562cd42dea1cc366c0005ca310bd
Sharick Hernandez <snhernandez79@ucatolica.edu.co>
2025-10-31 07:03:57 -0500

    crear autor

--- app/crud.py
+++ app/crud.py
@@ -3,3 +3,10 @@ from fastapi import HTTPException, status
 from typing import List
 from .models import Author, Book
 from .schemas import AuthorCreate, BookCreate
+
+def create_author(session: Session, author_in: AuthorCreate) -> Author:
+    author = Author.from_orm(author_in)
+    session.add(author)
+    session.commit()
+    session.refresh(author)
+    return author

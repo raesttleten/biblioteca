5c4813849404b57688bcb0579910eb1ce6d13022
Sharick Hernandez <snhernandez79@ucatolica.edu.co>
2025-10-31 06:49:58 -0500

    modelo de autor

--- app/models.py
+++ app/models.py
@@ -9,3 +9,12 @@ book_author_link = Table(
     Column("book_id", Integer, primary_key=True),
     Column("author_id", Integer, primary_key=True),
 )
+
+class AuthorBase(SQLModel):
+    name: str = Field(..., min_length=1, max_length=200)
+    country: Optional[str] = Field(None, max_length=100)
+    birth_year: Optional[int] = None
+
+class Author(AuthorBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    books: List["Book"] = Relationship(back_populates="authors", link_model=book_author_link)
